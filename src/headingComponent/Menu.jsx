b3851b4a88512547e8d83b20796ec0b179059c02
Dara-Chi <37431105+DaraHarris@users.noreply.github.com>
2020-05-02 21:10:34 +1000

    upcoming menu layout from Dara

--- src/headingComponent/Menu.jsx
+++ src/headingComponent/Menu.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 function Menu() {
   return (
-    <div className="menu">
-      <a href="#home">Add New Task</a>
-      <a href="#export">Export List</a>
-      <a href="#search">Complex Search</a>
+    <div className="menu" class="d-flex justify-content-center">
+      <a class="d-flex align-items-end mx-3" href="#home">Add New Task</a>
+      <a class="d-flex align-items-end mx-3"  href="#export">Export List</a>
+      <a class="d-flex align-items-end mx-3"  href="#search">Complex Search</a>
     </div>
   );
 }
--- src/headingComponent/SimpleSearch.jsx
+++ src/headingComponent/SimpleSearch.jsx
@@ -2,9 +2,21 @@ import React from "react";
 
 function SimpleSearch() {
   return (
-    <div className="simpleSearch">
-      <input type="text" placeholder="search" />
-    </div>
+   
+
+       <form className="simpleSearch">
+         
+          <div class="d-inline-flex">
+            <input class="mx-1" type="search" placeholder="search"/>
+            <button  type="submit">Search</button>
+          </div>
+
+      
+        
+      </form>
+
+     
+    
   );
 }
 
