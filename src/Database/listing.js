049ce07a3aa4f84c3f1156ae1faaabbedd02db1a
angeloMathews69 <48663321+angeloMathews69@users.noreply.github.com>
2020-05-07 14:56:18 +1000

    code organising and all task function with code comments

---
+++ src/Database/listing.js
@@ -0,0 +1,94 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "finishit_db",
+    
+}
+);
+
+    //This is the default function that should be called when user click on a date and toggle to "Alltasks"
+    function viewDefaultTask(){
+    var c = "2020-04-28 00:00:00";
+    
+    var sql ='select task.t_name,task.t_due_date,priority.p_priority from task INNER JOIN priority ON task.t_priority = priority.p_id where t_due_date = ' + mysql.escape(c) + 'ORDER BY task.t_priority'; 
+    con.query(sql, function (err, result){
+        if(err)
+        {
+            return console.log(err);
+        }
+        return console.log(result);
+    });
+    
+    }
+    
+    //when user toggle to onging task tab
+    function viewOngoingTask()
+    {
+    var c = "2020-04-28 00:00:00";
+    var sql ='select task.t_name,task.t_due_date,priority.p_priority from task INNER JOIN priority ON task.t_priority = priority.p_id where t_status = 1 AND t_due_date = ' + mysql.escape(c)+ 'ORDER BY task.t_priority'; 
+    con.query(sql, function (err, result){
+        if(err)
+        {
+            return console.log(err);
+        }
+        
+        return console.log(result);
+    });
+    }
+    
+    //when user toggle to toStart 
+    function viewToStartTask()
+    {
+    var c = "2020-04-28 00:00:00";
+    var sql ='select task.t_name,task.t_due_date,priority.p_priority from task INNER JOIN priority ON task.t_priority = priority.p_id where t_status = 2 AND t_due_date = ' + mysql.escape(c)+ 'ORDER BY task.t_priority'; 
+    con.query(sql, function (err, result){
+        if(err)
+        {
+            return console.log(err);
+        }
+        return console.log(result);
+    });
+    }
+    //when user toggle to donetask
+    function viewDoneTask()
+    {
+    var c = "2020-04-28 00:00:00";
+    var sql ='select task.t_name,task.t_due_date,priority.p_priority from task INNER JOIN priority ON task.t_priority = priority.p_id where t_status = 3 AND t_due_date = ' + mysql.escape(c)+ 'ORDER BY task.t_priority'; 
+    con.query(sql, function (err, result){
+        if(err)
+        {
+            return console.log(err);
+        }
+        return console.log(result);
+    });
+    }
+    //when user toggle to Overdue task
+    function viewOverdueTask()
+    {
+    var c = "2020-04-28 00:00:00";
+    var sql ='select task.t_name,task.t_due_date,priority.p_priority from task INNER JOIN priority ON task.t_priority = priority.p_id where t_status = 4 AND t_due_date = ' + mysql.escape(c)+ 'ORDER BY task.t_priority'; 
+    con.query(sql, function (err, result){
+        if(err)
+        {
+            return console.log(err);
+        }
+        return console.log(result);
+    });
+    
+    // //when user toggle to all tasks
+    // function viewAllTask()
+    // {
+    // var c = "2020-04-28 00:00:00";
+    // var sql ='select task.t_name,task.t_due_date,priority.p_priority from task INNER JOIN priority ON task.t_priority = priority.p_id where t_status = 5 AND t_due_date = ' + mysql.escape(c)+ 'ORDER BY task.t_priority'; 
+    // con.query(sql, function (err, result){
+    //     if(err)
+    //     {
+    //         return console.log(err);
+    //     }
+    //     return console.log(result);
+    // });
+    }
