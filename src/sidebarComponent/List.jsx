6c90a45576be55c0a87115dc812cedfa277867b6
Dara-Chi <37431105+DaraHarris@users.noreply.github.com>
2020-05-09 23:09:40 +1000

    scoll bar for list and tag

--- src/sidebarComponent/List.jsx
+++ src/sidebarComponent/List.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
 
@@ -6,14 +6,13 @@ function List(props) {
  
   return (
       <div className="mx-3">
-        <InputGroup size="sm" className="my-3 mx-1 row justify-content-between">
+        <InputGroup size="sm" className="my-3 mx-1 row justify-content-between ">
         <InputGroup.Text id="inputGroup-sizing-md"className="border border-0 bg-transparent px-0">List</InputGroup.Text>
         <InputGroup.Append>
           {/* onlclck to creating task url */}
           <Button variant="success" className="rounded-smj" onClick={() => props.addListItem("give a name")}>Add List</Button>
         </InputGroup.Append>
         </InputGroup>    
-       
       </div>     
   );
 }
