a594ed701606877ad92d794a7802ecc779c408f6
Dara-Chi <37431105+DaraHarris@users.noreply.github.com>
2020-05-09 21:59:06 +1000

    add Tag UI

--- src/sidebarComponent/List.jsx
+++ src/sidebarComponent/List.jsx
@@ -3,22 +3,18 @@ import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
 
 function List(props) {
+ 
   return (
       <div className="mx-3">
         <InputGroup size="sm" className="my-3 mx-1 row justify-content-between">
         <InputGroup.Text id="inputGroup-sizing-md"className="border border-0 bg-transparent px-0">List</InputGroup.Text>
         <InputGroup.Append>
           {/* onlclck to creating task url */}
-          <Button variant="success" className="rounded-sm" onClick={() => props.addListItem("give a name")}>Add List</Button>
+          <Button variant="success" className="rounded-smj" onClick={() => props.addListItem("give a name")}>Add List</Button>
         </InputGroup.Append>
         </InputGroup>    
        
-      </div>
-           
- 
-
-
-
+      </div>     
   );
 }
 
--- src/sidebarComponent/Tag.jsx
+++ src/sidebarComponent/Tag.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import InputGroup from 'react-bootstrap/InputGroup';
 import Button from "react-bootstrap/Button";
 
-function Tag() {
+function Tag(props) {
   return (
     <div className="mx-3">
          <InputGroup size="sm" className="my-3 mx-1 row justify-content-between">
         <InputGroup.Text id="inputGroup-sizing-md"className="border border-0 bg-transparent px-0">Tag</InputGroup.Text>
         <InputGroup.Append>
           {/* onlclck to creating task url */}
-          <Button variant="success" className="rounded-sm"
-            >Add Tag</Button>
+          <Button variant="success" className="rounded-sm" onClick={()=>{props.addTagItem("give a tag name")}}>Add Tag</Button>
         </InputGroup.Append>
       </InputGroup>
     </div> 
