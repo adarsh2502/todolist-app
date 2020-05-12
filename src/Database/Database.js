ca0d1ddb5d7fc5844697fbb8ad25af5f90cf9807
adarsh2502 <48426467+adarsh2502@users.noreply.github.com>
2020-05-12 13:29:11 +0530

    Add list database

--- src/Database/Database.js
+++ src/Database/Database.js
@@ -1,22 +1,11 @@
-var mysql = require('mysql');
+var mysql = require('mysql')
 
 var con = mysql.createConnection({
 
     host: "localhost",
     user: "root",
-    password: "",
+    password: "password",
     database: "finishit_db",
     
 }
 );
-//function listValues(){
-// var c = "2020-04-28 00:00:00";
-
-// var sql ='select * from task where t_due_date = ' + mysql.escape(c); 
-// con.query(sql, function (err, result){
-//     if(err)
-//     {
-//         return console.log(err);
-//     }
-//     return console.log(result);
-// });
